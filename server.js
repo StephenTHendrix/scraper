719c0e81bd0c9e8959b3d4ee72617cf20657aed9
StephenTHendrix <stephenthendrix@gmail.com>
2019-11-07 13:33:36 -0600

    added server.js, gitignore

---
+++ server.js
@@ -0,0 +1,39 @@
+// Using the tools and techniques you learned so far,
+// you will scrape a website of your choice, then place the data
+// in a MongoDB database. Be sure to make the database and collection
+// before running this exercise.
+
+// Consult the assignment files from earlier in class
+// if you need a refresher on Cheerio.
+
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Require axios and cheerio. This makes the scraping possible
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Initialize Express
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+// Database configuration
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+// Main route (simple Hello World Message)
+app.get("/", function(req, res) {
+  res.send("Hello world");
+});
+
+// Listen on port 3000
+app.listen(PORT, function() {
+  console.log("App running on port 3000!");
+});
